cf78e4d4a8a4aa2093a403c80ea6bce07c2696ec
John mayowa andrews <johnmayor723@yahoo.com>
2019-04-30 21:14:04 +0000

    refactor homes routes

--- handlers/homes.js
+++ handlers/homes.js
@@ -2,6 +2,39 @@ const Home = require('../models/homes')
 
 
 
+exports.showHomes = function(req, res){
+    //find the home with provided ID
+    Home.findById(req.params.id).populate("comments").exec(function(err, foundHome){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundHome)
+            //render show template with that home
+            res.render("homes/show", {home: foundHome});
+        }
+    });
+}
+
+exports.getNewHomes = function (req, res){
+    res.render('homes/new')
+}
+
+exports.createHomes = function(req, res){
+    // get data from form and add to campgrounds array
+    var title = req.body.title;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newHost = {title: title, image: image, description: desc}
+    // Create a new home and save to DB
+    Home.create(newHost, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to  homepage
+            res.redirect("/");
+        }
+    });
+}
 
 exports.searchHomes = function(req, res){
     
--- routes/homes.js
+++ routes/homes.js
@@ -1,50 +1,22 @@
 var express = require("express");
 var router  = express.Router();
 var Home = require("../models/homes");
-const { searchHomes } = require('../handlers/homes')
+const { searchHomes, createHomes, showHomes, newHomes, getNewHomes } = require('../handlers/homes')
 
 
 
 //CREATE - add new home to DB
-router.post("/new", function(req, res){
-    // get data from form and add to campgrounds array
-    var title = req.body.title;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newHost = {title: title, image: image, description: desc}
-    // Create a new home and save to DB
-    Home.create(newHost, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        } else {
-            //redirect back to  homepage
-            res.redirect("/");
-        }
-    });
-});
+router.post("/new", createHomes);
 
 // search homes routes
 
 router.get('/search', searchHomes)
 
 //NEW - show form to create new home
-/*router.get("/new", function(req, res){
-   res.render("homes/new"); 
-});*/
+router.get("/new", getNewHomes);
 
 // SHOW - shows more info about one home
-router.get("/:id", function(req, res){
-    //find the home with provided ID
-    Home.findById(req.params.id).populate("comments").exec(function(err, foundHome){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(foundHome)
-            //render show template with that home
-            res.render("homes/show", {home: foundHome});
-        }
-    });
-});
+router.get("/:id", showHomes);
 
 
 module.exports = router;
